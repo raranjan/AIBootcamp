f4bf68500022b6b0c7f3641794c63774f271058a
Rakesh Ranjan <raks.ranjan@gmail.com>
2020-02-15 22:29:24 +0530

    Generate data

---
+++ week_1/data_generator.py
@@ -0,0 +1,14 @@
+import math
+import sys
+import numpy as np
+
+def generate(n):
+    return [2*math.pi*i - np.random.randn() for i in range(n)]
+
+if __name__ == '__main__':
+    count = 10
+    if len(sys.argv) > 1:
+        count = sys.argv[1]
+
+    numbers = generate(count)
+    print('Generated Data for {} numbers: {}'.format(count, numbers))
