2429fa4f316e318bf62040df58b3dece572dd812
Rakesh Ranjan <raks.ranjan@gmail.com>
2020-02-15 22:50:44 +0530

    Updated generator function to calculate sin

--- week_1/data_generator.py
+++ week_1/data_generator.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 
 def generate(n):
-    return [2*math.pi*i - np.random.randn() for i in range(n)]
+    return [math.sin(2*math.pi*i) - np.random.randn() for i in range(n)]
 
 if __name__ == '__main__':
     count = 10
