369ba5b050f4c933366788bd7e5cd6ac6bdbbe04
Rakesh Ranjan <raks.ranjan@gmail.com>
2020-02-16 12:12:30 +0530

    Added menthod to generate data using numpy

--- week_1/data_generator.py
+++ week_1/data_generator.py
@@ -12,7 +12,25 @@ def generate(n):
     initial_data = np.random.random(n) # Generate random data between 0 and 1
     return [math.sin(2*math.pi*i) + np.random.randn() for i in initial_data]
 
-if __name__ == '__main__':
+def generate_with_np(n=10):
+    # Get equally spaced numbers using numpy
+    numbers = np.linspace(0, 1, n)
+
+    # Generate data based on formula
+    data = formula(numbers)
+
+    # Add noise to the data
+    data = data + noise(n)
+
+    return data
+
+def formula(X):
+    return np.sin(2*np.pi*X)
+
+def noise(n):
+    return np.random.normal(0, 1, n)
+
+def main():
     parser = argparse.ArgumentParser(description='A tutorial of argparse!')
     parser.add_argument("--n")
 
@@ -20,5 +38,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.n:
         count = int(args.n)
-    numbers = generate(count)
+    numbers = generate_with_np(count)
     print('Generated Data for {} numbers: {}'.format(count, numbers))
+    
+    generate_with_np()
+
+if __name__ == '__main__':
+    main()
